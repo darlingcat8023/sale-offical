4fb8451cc23b3bc0f055064d87a194413bad72c0
小温柔 <liujiajun8023@gmail.com>
2023-02-06 10:47:14 +0800

    Initial commit

---
+++ src/main/java/com/example/sale/SaleOfficalApplication.java
@@ -0,0 +1,13 @@
+package com.example.sale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleOfficalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleOfficalApplication.class, args);
+	}
+
+}
