f704c0c845c1f99c1eaf6adc561948c12e8ff7fa
小温柔 <liujiajun8023@gmail.com>
2023-02-06 10:48:49 +0800

    update

--- src/main/java/com/example/sale/SaleOfficalApplication.java
+++ src/main/java/com/example/sale/SaleOfficalApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SaleOfficalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaleOfficalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SaleOfficalApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/sale/SaleOfficalApplicationTests.java
+++ src/test/java/com/example/sale/SaleOfficalApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SaleOfficalApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
